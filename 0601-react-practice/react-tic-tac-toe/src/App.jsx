fc039be5b295d5c13800d253e651d07fbb0910cc
延岡あやか <nobeokaayaka@nobeokaayakanoMacBook-Air.local>
2025-06-02 22:54:12 +0900

    コミット！

---
+++ 0601-react-practice/react-tic-tac-toe/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import "../src/styles/App.css";
+import Square from "./components/Square";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>三目並べ</h1>
+        <Square />
+      </div>
+    </>
+  );
+}
+
+export default App;
